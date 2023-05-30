71935d9822a37816b921fd3df3bc22bb42505326
Ramiro Couceiro <coucerama@gmail.com>
2023-05-30 02:52:21 -0300

    routes

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -9,41 +9,60 @@ import {
 } from "./ProductsStyles";
 import axios from "axios";
 import ProductCard from "./ProductCard";
+import pizzas from "../../food/pizzas.json";
+import breads from "../../food/breads.json";
+import chocolates from "../../food/chocolates.json";
+import drinks from "../../food/drinks.json";
+import burgers from "../../food/burgers.json";
 
 const Products = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState(pizzas);
   const [category, setCategory] = useState("Pizzas");
-  const getProducts = async () => {
-    await axios.get(`./food/${category}.json`).then((res) => {
-      setProducts(res.data);
-    });
-  };
-
-  useEffect(() => {
-    getProducts();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [category]);
-
   return (
     <MenuSection>
       <CategoryContainer>
-        <CategoryBtn onClick={() => setCategory("Pizzas")}>
+        <CategoryBtn
+          onClick={() => {
+            setProducts(pizzas);
+            setCategory("Pizzas");
+          }}
+        >
           <img src='./img/porcion-de-pizza.png' alt='' />
           Pizza
         </CategoryBtn>
-        <CategoryBtn onClick={() => setCategory("Breads")}>
+        <CategoryBtn
+          onClick={() => {
+            setProducts(breads);
+            setCategory("Breads");
+          }}
+        >
           <img src='./img/panes.png' alt='' />
           Breads
         </CategoryBtn>
-        <CategoryBtn onClick={() => setCategory("Chocolates")}>
+        <CategoryBtn
+          onClick={() => {
+            setProducts(chocolates);
+            setCategory("Chocolates");
+          }}
+        >
           <img src='./img/chocolate.png' alt='' />
           Chocolates
         </CategoryBtn>
-        <CategoryBtn onClick={() => setCategory("Drinks")}>
+        <CategoryBtn
+          onClick={() => {
+            setProducts(drinks);
+            setCategory("Drinks");
+          }}
+        >
           <img src='./img/martini.png' alt='' />
           Drinks
         </CategoryBtn>
-        <CategoryBtn onClick={() => setCategory("Burgers")}>
+        <CategoryBtn
+          onClick={() => {
+            setProducts(burgers);
+            setCategory("Burgers");
+          }}
+        >
           <img src='./img/burguer.png' alt='' />
           Burgers
         </CategoryBtn>
